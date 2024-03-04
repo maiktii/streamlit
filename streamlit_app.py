87e41a66a6ae949a94786c6153c7e71fa0c70937
Michael The <118290644+maiktii@users.noreply.github.com>
2024-03-04 20:45:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,6 @@ st.header('Ini percobaan Streamlit di Google Colab')
 
 days_df = pd.read_csv("Bike-sharing-dataset/day.csv")
 
-min_date = days_df["dteday"].min()
-max_date = days_df["dteday"].max()
- 
-with st.sidebar:
-    # Menambahkan logo perusahaan
-    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
-    
-    # Mengambil start_date & end_date dari date_input
-    start_date, end_date = st.date_input(
-        label='Rentang Waktu',min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
-    )
-
 filtered_data = days_df[(days_df["yr"] == 1) & (days_df["workingday"] == 0)]
 
 filtered_data_2 = days_df[(days_df["workingday"] == 1) | (days_df["workingday"] == 0)]
@@ -34,15 +20,8 @@ grouped_data_2 = filtered_data_2.groupby(['workingday', 'weathersit'])['cnt'].me
 fig, ax = plt.subplots(figsize=(10,6))
 ax.plot(
     grouped_data["dteday"], grouped_data["cnt"]["max"],
-    label="Maximum Count",
-    marker='o',
-    linewidth=2,
-    color="#72BCD4"
-)
-
-ax.plot(
     grouped_data["dteday"], grouped_data["cnt"]["min"],
-    label="Minimum Count",
+    label="Maximum Count",
     marker='o',
     linewidth=2,
     color="#72BCD4"
